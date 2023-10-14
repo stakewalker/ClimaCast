b64954438959d51ae57392cd926466ab6a051a79
Alexandre Stakonski <115736059+stakewalker@users.noreply.github.com>
2023-10-14 16:49:17 +0000

    Create env.py

---
+++ env.py
@@ -0,0 +1,6 @@
+# It's a simple solution, feel free to do auth in other way
+clima = "insert-key-here"
+tl_key = "insert-key-here"
+chat_id = "insert-here"
+city_id = "insert-here"
+openai_key = "insert-key-here"
